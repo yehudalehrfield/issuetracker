13bbd3af3517a0485ef85766edd31720deb31150
Yehuda Lehrfield <yll2113@columbia.edu>
2022-12-15 20:44:15 -0500

    fix: mongoose schema

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ try {
 // define new schema
 const issueSchema = new mongoose.Schema({
   issue_title: { type: String, required: true },
-  issue_text: String,
+  issue_text: { type: String, required: true },
   created_on: { type: Date, required: true },
   updated_on: Date,
   created_by: String,
